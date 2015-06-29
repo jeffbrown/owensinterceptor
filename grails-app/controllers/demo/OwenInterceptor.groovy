9a2a2d1307d0d1c0ceda969a233fbbe1de080da2
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-06-28 23:27:06 -0500

    Use GrailsConfigurationAware

--- grails-app/controllers/demo/OwenInterceptor.groovy
+++ grails-app/controllers/demo/OwenInterceptor.groovy
@@ -1,6 +1,16 @@
 package demo
 
-class OwenInterceptor {
+import grails.core.support.GrailsConfigurationAware
+import grails.config.Config
+
+class OwenInterceptor implements GrailsConfigurationAware {
+
+    void setConfiguration(Config cfg) {
+        def controllerToIntercept = cfg.grails.owen.controllerToIntercept
+        match controller: controllerToIntercept
+
+        // initialize whatever other properties necessary
+    }
 
     boolean before() {
         println 'Interceptor Running...'
--- grails-app/init/owensinterceptor/Application.groovy
+++ grails-app/init/owensinterceptor/Application.groovy
@@ -4,14 +4,6 @@ import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
 class Application extends GrailsAutoConfiguration {
-
-    @Override
-    void doWithApplicationContext() {
-        def interceptor = applicationContext.owenInterceptor
-        def controllerToIntercept = applicationContext.grailsApplication.config.grails.owen.controllerToIntercept
-        interceptor.match controller: controllerToIntercept
-    }
-
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
